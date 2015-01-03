a098e634dd9d5694f444bc5f1eaaf9523a1a1a60
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-01-02 23:12:30 -0800

    More source fitting improvements
    I'm beginning to get an idea of how I want to implement source fitting.

--- src/selfcal.jl
+++ src/selfcal.jl
@@ -1,9 +1,30 @@
 @doc """
 Fit the visibilities to a model of point sources. The input model needs
 to have the positions of the point sources relatively close, but the flux
-can be wildly off. This function returns a list of the fitted fluxes
-and positions at each frequency.
+can be wildly off.
 """ ->
+function fitvisibilities(frame::ReferenceFrame,
+                         data::Array{Complex64,3},
+                         sources::Vector{Source},
+                         u,v,w,ν)
+    # TODO: Sort sources by flux before doing this.
+    # The idea is that small errors on bright sources (looking at you Cyg A)
+    # will propagate as irrecoverable large errors on fainter sources.
+    # Therefore we want to make sure we get the bright sources right first.
+    for i = 1:2
+        sources_omitting_current = Source[]
+        for j = 1:length(sources)
+            i == j && continue
+            push!(sources_omitting_current,sources[j])
+        end
+        model = visibilities(frame,sources_omitting_current,u,v,w,ν)
+        data_minus_model = data-model
+        newsource = fitvisibilities(frame,data_minus_model,
+                                    sources[i],u,v,w,ν)
+        sources[i] = newsource
+    end
+end
+
 function fitvisibilities(frame::ReferenceFrame,
                          data::Array{Complex64,3},
                          source::Source,
@@ -19,13 +40,19 @@ function fitvisibilities(frame::ReferenceFrame,
 
     # TODO: apply flags and baseline filtering
 
-    #for β = 1:Nfreq
-    for β = 1:1
+    lfit = Array(Float64,Nfreq)
+    mfit = Array(Float64,Nfreq)
+    fluxfit = Array(Float64,Nfreq)
+    for β = 1:Nfreq
         flux = getflux(source,ν[β])
-        fitvisibilities(slice(data,:,β,1),
-                        slice(fringe,:,β),
-                        flux,l,m,u,v,w,ν[β])
+        l_,m_,flux_ = fitvisibilities(slice(data,:,β,1),
+                                      slice(fringe,:,β),
+                                      flux,l,m,u,v,w,ν[β])
+        lfit[β] = l_
+        mfit[β] = m_
+        fluxfit[β] = flux_
     end
+    fitsource(frame,source.name,ν,lfit,mfit,fluxfit)
 end
 
 function fitvisibilities(data,fringe,flux,l,m,u,v,w,ν)
@@ -57,5 +84,29 @@ function fitvisibilities(data,fringe,flux,l,m,u,v,w,ν)
     m += Δparam[2]
     flux += Δparam[3]
     l,m,flux
+end
+
+function fitsource(frame,name,ν,l,m,flux)
+    # Get the average position of each source
+    # TODO: weight higher frequencies more (higher resolution)
+    lnew = mean(l)
+    mnew = mean(m)
+
+    az = atan2(lnew,mnew)
+    el = acos(sqrt(lnew.^2+mnew.^2))
+
+    azel  = Direction("AZEL",Quantity(az,"rad"),Quantity(el,"rad"))
+    j2000 = measure(frame,azel,"J2000")
+
+    # Fit a 2-component spectrum to the source
+    reffreq = 47e6
+    x = log10(ν/reffreq)
+    y = log10(flux)
+    z = [ones(length(ν)) x x.^2]\y
+    logflux   = z[1]
+    index     = z[2:3]
+    amplitude = 10.0.^logflux
+
+    Source(name,j2000.m[1],j2000.m[2],amplitude,reffreq,index)
 end
 
