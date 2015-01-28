2596d762fc69fcd13677ede1184e4647cb31a8a8
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-01-28 10:56:59 -0800

    Fix flagdata!

--- src/flagdata.jl
+++ src/flagdata.jl
@@ -4,15 +4,14 @@
 @doc """
 Apply antenna flags to the measurement set.
 """ ->
-function flagdata!(ms::Table,interferometer::Interferometer)
+function flagdata!(ms::Table,flaggedantennas::Vector{Int})
     N = numrows(ms)
-    # (the +1 converts to a 1-based indexing scheme)
-    ant1 = ms["ANTENNA1"]+1
-    ant2 = ms["ANTENNA2"]+1
-    flags = zeros(Bool,4,channels(interferometer),N)
-    for i = 1:N
-        if ant1[i] in flaggedantennas(interferometer) || ant2[i] in flaggedantennas(interferometer)
-            flags[:,:,i] = true
+    Nfreq = length(freq(ms))
+    ant1,ant2 = ants(ms)
+    flags = zeros(Bool,4,Nfreq,N)
+    for α = 1:N
+        if ant1[α] in flaggedantennas || ant2[α] in flaggedantennas
+            flags[:,:,α] = true
         end
     end
     ms["FLAG"] = flags
