0d9511d7c4f31b394db653d594fdcd2e20a73084
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-01-28 15:09:35 -0800

    Completely delete the run(...) interface
    (pending a replacement)

--- src/TTCal.jl
+++ src/TTCal.jl
@@ -1,7 +1,6 @@
 module TTCal
 
 export Source
-
 export genvis
 export fitvis
 export subsrc!
@@ -29,48 +28,6 @@ include("subsrc.jl")
 include("flagdata.jl")
 include("bandpass.jl")
 include("applycal.jl")
-
-#=
-function run(args)
-    ms = [Table(ASCIIString(input)) for input in args["measurementsets"]]
-
-    Nbase = numrows(ms[1])
-    Nant  = round(Int,(sqrt(8Nbase+1)-1)/2)
-    spw = Table(ms[1][kw"SPECTRAL_WINDOW"])
-    Nfreq = length(spw["CHAN_FREQ",1])
-    lwa = Interferometer(Nant,Nfreq,args["refant"],args["flags"])
-
-    applycal_flag = args["applycal"]
-    if applycal_flag
-        gains = load(args["gaintable"])
-        applycal(lwa,ms,gains)
-    else
-        frame = ReferenceFrame()
-        set!(frame,Epoch("UTC",ms[1]["TIME",1]*Second))
-        set!(frame,Measures.observatory(frame,"OVRO_MMA"))
-        sources  = getsources(frame)
-        criteria = StoppingCriteria(args["niter"],
-                                    args["tol"])
-        gains = bandpass(lwa,ms,sources,criteria)
-        save(args["gaintable"],gains)
-    end
-    nothing
-end
-=#
-
-@doc """
-Save the gains as a numpy array.
-""" ->
-function save(filename,gains)
-    npzwrite(filename,gains)
-    nothing
-end
-@doc """
-Load the gains from a numpy array.
-""" ->
-function load(filename)
-    npzread(filename)
-end
 
 end
 
