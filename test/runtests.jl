5e13e1c134e9e795fe5070adf77cdfd84f19518c
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-03-18 18:17:37 -0700

    Fix up the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -89,7 +89,7 @@ const frame = ReferenceFrame()
 set!(frame,Epoch("UTC",t))
 set!(frame,Measures.observatory(frame,"OVRO_MMA"))
 
-const sources = filter(source -> TTCal.isabovehorizon(frame,source),TTCal.readsources("sources.json"))
+const sources = filter(source -> TTCal.isabovehorizon(frame,source),readsources("sources.json"))
 
 function test_bandpass(gains,data,model)
     mygains = similar(gains)
