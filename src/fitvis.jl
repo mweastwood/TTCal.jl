6d951ef1f04f0cfd8b52139f08224c04457185cc
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-04-10 14:36:39 -0700

    Large changes to position fitting code of fitvis

--- src/fitvis.jl
+++ src/fitvis.jl
@@ -22,16 +22,15 @@ to have the positions of the point sources relatively close, but the flux
 can be wildly off.
 """ ->
 function fitvis(ms::Table,
-                sources::Vector{PointSource},
-                criteria::StoppingCriteria)
+                sources::Vector{PointSource})
     frame = reference_frame(ms)
-    data  = ms["CORRECTED_DATA"]
+    data  = Tables.checkColumnExists(ms,"CORRECTED_DATA")? ms["CORRECTED_DATA"] : ms["DATA"]
     flags = ms["FLAG"]
     u,v,w = uvw(ms)
     ν = freq(ms)
     ant1,ant2 = ants(ms)
 
-    fitvis(frame,data,flags,u,v,w,ν,ant1,ant2,sources,criteria)
+    fitvis(frame,data,flags,u,v,w,ν,ant1,ant2,sources)
 end
 
 ################################################################################
@@ -51,180 +50,142 @@ function fitvis(frame::ReferenceFrame,
                 ν::Vector{quantity(Float64,Hertz)},
                 ant1::Vector{Int32},
                 ant2::Vector{Int32},
-                sources::Vector{PointSource},
-                criteria::StoppingCriteria)
-    # Flag short baselines?
-    for α = 1:length(u)
-        if sqrt(u[α]^2+v[α]^2+w[α]^2) < 50Meter
-            flags[:,:,α] = true
-        end
-    end
-
+                sources::Vector{PointSource})
     # 1. Discard sources that are below the horizon.
     sources = filter(sources) do source
         isabovehorizon(frame,source)
     end
-    @show sources
 
     # 2. Sort the sources in order of decreasing flux.
-    sort!(sources, by=source->source.flux, rev=true)
-    @show sources
-
-    # 3. Convert all the sources to `RFISources`
-    # (this removes the spectral smoothness constraint)
-    output_sources = [RFISource(frame,source,ν) for source in sources]
-    @show output_sources
+    #sort!(sources, by=source->source.I, rev=true)
 
     # 3. For each source:
-    for i = 1:length(output_sources)
-        @show i
+    for i = 1:length(sources)
+        name = sources[i].name
+        l,m = getlm(frame,sources[i])
+        lold = l; mold = m # TEMP
+
         # a) Subtract all other sources.
-        other_sources = RFISource[]
-        for j = 1:length(output_sources)
+        other_sources = PointSource[]
+        for j = 1:length(sources)
             i == j && continue
-            push!(other_sources,output_sources[j])
+            push!(other_sources,sources[j])
         end
         subtracted = subsrc(frame,data,u,v,w,ν,other_sources)
 
-        # b) Obtain the source's spectrum.
-        l,m = lm(frame,output_sources[i])
-        @time spectrum = getspec_internal(subtracted,flags,l,m,u,v,w,ν,ant1,ant2)
-        xx = squeeze(spectrum[1,:],1)
-        yy = squeeze(spectrum[4,:],1)
-        I = 0.5*(xx + yy)
-
-        # c) Fit for the source's position.
-        @time l,m = fitvis_outer(subtracted,flags,l,m,u,v,w,ν,ant1,ant2,spectrum,criteria)
+        # b) Solve for the source's position
+        l,m = fitvis_lm(subtracted,flags,l,m,u,v,w,ν,ant1,ant2)
         az,el = lm2azel(l,m)
-        azel  = Direction("AZEL",az,el)
 
-        # d) Update the source.
-        output_sources[i] = RFISource(output_sources[i].name,azel,I,ν)
+        # c) Solve for the source's spectrum
+        I,Q,U,V,reffreq,index = fitvis_spec(subtracted,flags,l,m,u,v,w,ν,ant1,ant2)
+
+        #println("----")
+        #@show name lold,l mold,m sources[i].I,I sources[i].Q,Q sources[i].U,U sources[i].V,V sources[i].index,index
+        sources[i] = PointSource(name,Direction("AZEL",az,el),
+                                 I,Q,U,V,reffreq,index)
     end
-    output_sources
+    sources
 end
 
-@doc """
-Take Runge-Kutta steps until the position converges.
-""" ->
-function fitvis_outer(data::Array{Complex64,3},
-                      flags::Array{Bool,3},
-                      l::Float64,
-                      m::Float64,
-                      u::Vector{quantity(Float64,Meter)},
-                      v::Vector{quantity(Float64,Meter)},
-                      w::Vector{quantity(Float64,Meter)},
-                      ν::Vector{quantity(Float64,Hertz)},
-                      ant1::Vector{Int32},
-                      ant2::Vector{Int32},
-                      spectrum::Array{Float64,2},
-                      criteria::StoppingCriteria)
-    position = [l,m]
-    oldposition = copy(position)
-    workspace = RKWorkspace(position,2)
-
-    # Take Runge-Kutta steps until the position converges.
-    iter = 0
-    converged = false
-    while !converged && iter < criteria.maxiter
-        oldposition[:] = position
-        @time rkstep!(position,workspace,fitvis_step!,data,flags,u,v,w,ν,ant1,ant2,spectrum)
-
-        @show (position[1] - oldposition[1]) (position[2] - oldposition[2])
-        if abs(position[1] - oldposition[1]) < criteria.tolerance &&
-           abs(position[2] - oldposition[2]) < criteria.tolerance
-            converged = true
-        end
-        iter += 1
-    end
-
-    l = position[1]
-    m = position[2]
-    l,m
-end
-
-@doc """
-1. Fit for the position and flux of the source at each frequency.
-2. Get a single position for the source.
-""" ->
-function fitvis_inner!(output::Vector{Float64},
-                       input::Vector{Float64},
-                       data::Array{Complex64,3},
-                       flags::Array{Bool,3},
-                       u::Vector{quantity(Float64,Meter)},
-                       v::Vector{quantity(Float64,Meter)},
-                       w::Vector{quantity(Float64,Meter)},
-                       ν::Vector{quantity(Float64,Hertz)},
-                       ant1::Vector{Int32},
-                       ant2::Vector{Int32},
-                       spectrum::Array{Float64,2})
-    l = input[1]
-    m = input[2]
-    n = sqrt(1-l^2-m^2)
-    Nbase = length(u)
+function fitvis_lm(data::Array{Complex64,3},
+                   flags::Array{Bool,3},
+                   l::Float64,
+                   m::Float64,
+                   u::Vector{quantity(Float64,Meter)},
+                   v::Vector{quantity(Float64,Meter)},
+                   w::Vector{quantity(Float64,Meter)},
+                   ν::Vector{quantity(Float64,Hertz)},
+                   ant1::Vector{Int32},
+                   ant2::Vector{Int32})
     Nfreq = length(ν)
+    Nbase = length(u)
+    λ = [c/ν[β] for β = 1:Nfreq]
+
+    # Calculate the flux in the given direction
+    # (and its derivatives with respect to direction)
 
     fringe = fringepattern(l,m,u,v,w,ν)
-    delta  = zeros(Complex64,4Nbase)
-    # Columns contain: [∂V/∂l ∂V/∂m]
-    # Where V is the contribution to the visibility of a single point source,
-    #       and l and m specify the position of the point source.
-    matrix = Array(Complex64,4Nbase,2)
-    δl = Array(Float64,Nfreq)
-    δm = Array(Float64,Nfreq)
-
-    # 1. Fit for the position of the source at each frequency.
-    @inbounds for β = 1:Nfreq
-        λ = c/ν[β]
-        count = 0 # The number of unflagged visibilities
-        for α = 1:length(u)
-            ant1[α] == ant2[α] && continue
-            (flags[1,β,α] || flags[2,β,α] || flags[3,β,α] || flags[4,β,α]) && continue
-
-            # Note: constraining the complex conjugates as well forces l and m
-            # to be real-valued.
-
-            model_xx = 0.5*spectrum[1,β]*fringe[β,α]
-            model_yy = 0.5*spectrum[4,β]*fringe[β,α]
-
-            dfringe_l = 2.0im*π*(u[α].val-w[α].val*l/n)/λ.val
-            dfringe_m = 2.0im*π*(v[α].val-w[α].val*m/n)/λ.val
-
-            delta[4count+1] = data[1,β,α] - model_xx
-            delta[4count+2] = data[4,β,α] - model_yy
-            delta[4count+3] = conj(delta[4count+1])
-            delta[4count+4] = conj(delta[4count+2])
-
-            matrix[4count+1,1] = dfringe_l * model_xx
-            matrix[4count+2,1] = dfringe_l * model_yy
-            matrix[4count+3,1] = conj(matrix[4count+1,1])
-            matrix[4count+4,1] = conj(matrix[4count+2,1])
-
-            matrix[4count+1,2] = dfringe_m * model_xx
-            matrix[4count+2,2] = dfringe_m * model_yy
-            matrix[4count+3,2] = conj(matrix[4count+1,2])
-            matrix[4count+4,2] = conj(matrix[4count+2,2])
-
-            count += 1
+    F   = zeros(Nfreq)     # flux at each frequency
+    dF  = zeros(2,Nfreq)   # [ ∂F/∂l ∂F/∂m ]
+    d2F = zeros(2,2,Nfreq) # [ ∂²F/∂l²  ∂²F/∂l∂m
+                           #   ∂²F/∂l∂m ∂²F/∂m² ]
+    count  = zeros(Int,Nfreq) # The number of baselines used in the calculation
+    @inbounds for α = 1:Nbase
+        # Don't use auto-correlations
+        ant1[α] == ant2[α] && continue
+        for β = 1:Nfreq
+            any(slice(flags,:,β,α)) && continue
+            z = conj(fringe[β,α])
+            V = 0.5*(data[1,β,α]+data[4,β,α])*z
+            uu = u[α]/λ[β]
+            vv = v[α]/λ[β]
+            F[β]       += real(V)
+            dF[1,β]    += real(V * -2im*π * uu)
+            dF[2,β]    += real(V * -2im*π * vv)
+            d2F[1,1,β] += real(V * (-2im*π)^2 * uu^2)
+            d2F[2,1,β] += real(V * (-2im*π)^2 * uu*vv)
+            d2F[1,2,β] += real(V * (-2im*π)^2 * uu*vv)
+            d2F[2,2,β] += real(V * (-2im*π)^2 * vv^2)
+            count[β] += 1
         end
-        params = sub(matrix,1:4count,:)\sub(delta,1:4count)
-        δl[β] = real(params[1])
-        δm[β] = real(params[2])
+    end
+    for β = 1:Nfreq
+        F[β] /= count[β]
+        dF[:,β] /= count[β]
+        d2F[:,:,β] /= count[β]
     end
 
-    # 2. Get a single position for the source.
-    # (Weight the positions by ν^2 because resolution ~ 1/ν)
-    weights = ν.^2
-    ΔL = sum(weights .* δl) ./ sum(weights)
-    ΔM = sum(weights .* δm) ./ sum(weights)
-    @show ΔL ΔM
-
-    output[1] = ΔL
-    output[2] = ΔM
-    nothing
+    # Calculate how far to step in each direction
+    dl = 0.0
+    dm = 0.0
+    normalization = 0.0
+    for β = 1:Nfreq
+        count[β] .== 0 && continue
+        δ = slice(d2F,:,:,β) \ slice(dF,:,β)
+        weight = count[β]/(λ[β]/Meter)
+        dl += δ[1] * weight
+        dm += δ[2] * weight
+        normalization += weight
+    end
+    dl /= normalization
+    dm /= normalization
+    force_to_horizon(l-dl,m-dm)
 end
 
-const fitvis_step! = RKInnerStep{:fitvis_inner!}()
+function fitvis_spec(data::Array{Complex64,3},
+                     flags::Array{Bool,3},
+                     l::Float64,
+                     m::Float64,
+                     u::Vector{quantity(Float64,Meter)},
+                     v::Vector{quantity(Float64,Meter)},
+                     w::Vector{quantity(Float64,Meter)},
+                     ν::Vector{quantity(Float64,Hertz)},
+                     ant1::Vector{Int32},
+                     ant2::Vector{Int32})
+    I,Q,U,V = getspec_internal(data,flags,l,m,u,v,w,ν,ant1,ant2)
+
+    # power law fit
+    mask = (!isnan(I)) .* (I .> 0)
+    reffreq = 47e6Hertz
+    if sum(mask) > 0.5length(mask)
+        # Give a reasonable anser if there are too many channels masked
+        α = [0.0 0.0]
+    else
+        x = log10(ν[mask]/reffreq)
+        y = log10(I[mask])
+        α = [ones(length(x)) x]\y
+    end
+
+    # polarization fractions
+    # (this is messing things up somehow)
+    fQ = mean(Q[mask]./I[mask])
+    fU = mean(U[mask]./I[mask])
+    fV = mean(V[mask]./I[mask])
+
+    flux = 10^α[1]
+    flux,fQ*flux,fU*flux,fV*flux,reffreq,α[2:end]
+end
 
 @doc """
 This function forces l and m to be above the horizon.
