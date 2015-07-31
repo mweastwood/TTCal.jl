4d136b88557a1c5a58cd99c445bbcb50f34a39fe
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-07-31 15:04:36 -0700

    Use JLD for calibration i/o

--- src/io.jl
+++ src/io.jl
@@ -13,46 +13,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-function write_gains(filename,gains::Array{Complex64,3},gain_flags::Array{Bool,3})
-    # gains are from bandpass(...)
-    Nant,Npol,Nchan = size(gains)
-    open(filename,"w") do f
-        write(f,'B')
-        write(f,Int32(Nant))
-        write(f,Int32(Nchan))
-        write(f,permutedims(gain_flags,(2,3,1)))
-        write(f,permutedims(gains,(2,3,1)))
-    end
+using JLD
+
+function write(filename,calibration::Calibration)
+    save(filename,"cal",calibration)
 end
 
-function write_gains(filename,gains::Array{Complex128,4},gain_flags::Array{Bool,2})
-    # gains are from polcal(...)
-    Npol1,Npol2,Nant,Nchan = size(gains)
-    open(filename,"w") do f
-        write(f,'J')
-        write(f,Int32(Nant))
-        write(f,Int32(Nchan))
-        write(f,permutedims(gain_flags,(2,1)))
-        write(f,permutedims(gains,(1,2,4,3)))
-    end
-end
-
-function read_gains(filename)
-    local gains,gain_flags
-    open(filename,"r") do f
-        T = read(f,Char)
-        Nant = Int(read(f,Int32))
-        Nchan = Int(read(f,Int32))
-        if T == 'B'
-            gain_flags = permutedims(read(f,Bool,(2,Nchan,Nant)),(3,1,2))
-            gains = permutedims(read(f,Complex64,(2,Nchan,Nant)),(3,1,2))
-        elseif T == 'J'
-            gain_flags = permutedims(read(f,Bool,(Nchan,Nant)),(2,1))
-            gains = permutedims(read(f,Complex128,(2,2,Nchan,Nant)),(1,2,4,3))
-        else
-            error("Unknown calibration type.")
-        end
-    end
-    gains,gain_flags
+function read(filename)
+    load(filename,"cal")
 end
 
