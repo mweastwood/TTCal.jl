8b7c3a95b92e94b55e71148be5aa4305645ddb62
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-09-29 09:57:44 -0700

    Fix the LWA memo 178 beam model to not depend on an external file
    Also alters the code to use the built-in at-evalpoly macro

--- src/beammodel.jl
+++ src/beammodel.jl
@@ -30,57 +30,65 @@ end
 
 ####################################################################
 # J. Dowell empirical beam model, function of frequency
+
+const _E178 = [-4.529931167425190e+01 -3.066691727279789e+01 +7.111192148086860e+01 +1.131338637814271e+01
+               +1.723596273204143e+02 +1.372536555724785e+02 -2.664504470520252e+02 -3.493942140370373e+01
+               -2.722311453669980e+02 -2.368121452949910e+02 +4.343953141004854e+02 +5.159758406047006e+01
+               +2.408402047219155e+02 +2.302040670131884e+02 -3.993175413350526e+02 -4.152532958513045e+01
+               -1.334589043702679e+02 -1.414814115813401e+02 +2.312015990805256e+02 +2.016753495756661e+01
+               +4.917278320096442e+01 +5.820262041648866e+01 -8.952191930147437e+01 -6.170044495806915e+00
+               -1.244683117802972e+01 -1.652458005311876e+01 +2.396407975769994e+01 +1.185153343629695e+00
+               +2.195017889732819e+00 +3.281357788749097e+00 -4.500463893128127e+00 -1.311234364955677e-01
+               -2.690339381925372e-01 -4.547844648614167e-01 +5.919912952153034e-01 +4.850030701166225e-03
+               +2.243604641077215e-02 +4.311447324076417e-02 -5.345256010140458e-02 +7.056536178294128e-04
+               -1.211643367267544e-03 -2.665185970775750e-03 +3.157455237232649e-03 -1.102532965104807e-04
+               +3.810179416998095e-05 +9.681257383030349e-05 -1.099243217364324e-04 +6.250327364174989e-06
+               -5.277176362640874e-07 -1.567746596003443e-06 +1.710529173897111e-06 -1.350506188926788e-07]
+
+const _H178 = [+4.062920357822495e+02 +3.038713068453467e+01
+               -1.706845366994521e+03 -1.337217221068207e+02
+               +3.095438045596764e+03 +2.567017051330929e+02
+               -3.164514198869798e+03 -2.757538828204631e+02
+               +2.035008485840167e+03 +1.850998851042284e+02
+               -8.713893456840954e+02 -8.227855478515090e+01
+               +2.564477521133275e+02 +2.502479051323083e+01
+               -5.262251671400085e+01 -5.287316195660030e+00
+               +7.519512104996896e+00 +7.754610944702455e-01
+               -7.337746902310935e-01 -7.744604652809532e-02
+               +4.663414310713179e-02 +5.024254273102379e-03
+               -1.740005497709271e-03 -1.908989166200470e-04
+               +2.892116885178882e-05 +3.223985512686652e-06]
+
 """
 Generate beam model to attenuate source model flux (to be used in genvis.jl).
 Using the J. Dowell beam model (http://www.faculty.ece.vt.edu/swe/lwa/memo/lwa0178a.pdf).
 """
-file    = jldopen("/scr/mmanders/LWA/ttcal_tutorial/lwa1-dipole-emp.jld", "r")
-jdmodel = JLD.read(file, "jdmodel")
-fitX    = jdmodel["fitX"]
-fitY    = jdmodel["fitY"]
-
 function beammodel(frame,phase_dir,l,m,ν)
     dir   = lm2dir(phase_dir,l,m)
     az,el = dir2azel(frame,dir)
     # zenith angle
     ze    = π/2-el
 
-    px = sqrt( ( planeE(fitX,ze,ν) * cos(az) )^2 + ( planeH(fitX,ze,ν) * sin(az) )^2 )
-    py = sqrt( ( planeE(fitY,ze,ν) * cos(az) )^2 + ( planeH(fitY,ze,ν) * sin(az) )^2 )
+    px = sqrt( ( planeE(ze,ν) * cos(az) )^2 + ( planeH(ze,ν) * sin(az) )^2 )
+    py = sqrt( ( planeE(ze,ν) * sin(az) )^2 + ( planeH(ze,ν) * cos(az) )^2 )
     p  = (px + py) / 2
     p
 end
 
-function planeE(fitPol,ze,ν)
-    alpha = polyval(fitPol[1,1,:],ν)
-    beta  = polyval(fitPol[1,2,:],ν)
-    gamma = polyval(fitPol[1,3,:],ν)
-    delta = polyval(fitPol[1,4,:],ν)
-
-    out = (1 - (2*ze/π)^alpha) * cos(ze)^beta
-    out += gamma * (2*ze/π) * cos(ze)^delta
-    out
+@eval function planeE(ze,ν)
+    x = ν / 10e6
+    α = @evalpoly(x,$(_E178[:,1]...))
+    β = @evalpoly(x,$(_E178[:,2]...))
+    γ = @evalpoly(x,$(_E178[:,3]...))
+    δ = @evalpoly(x,$(_E178[:,4]...))
+    (1-(ze/(π/2))^α)*cos(ze)^β + γ*(ze/(π/2))*cos(ze)^δ
 end
 
-function planeH(fitPol,ze,ν)
-    alpha = polyval(fitPol[2,1,:],ν)
-    beta  = polyval(fitPol[2,2,:],ν)
-    gamma = polyval(fitPol[2,3,:],ν)
-    delta = polyval(fitPol[2,4,:],ν)
-    
-    out = (1 - (2*ze/π)^alpha) * cos(ze)^beta
-    out += gamma * (2*ze/π) * cos(ze)^delta
-    out
+@eval function planeH(ze,ν)
+    x = ν / 10e6
+    α = @evalpoly(x,$(_H178[:,1]...))
+    β = @evalpoly(x,$(_H178[:,2]...))
+    # γ and δ are neglected on the H plane
+    (1-(ze/(π/2))^α)*cos(ze)^β
 end
 
-
-"""
-Evaluate polynomial of degree N-1 where N = len(coeffs).
-"""
-function polyval(coeffs,param)
-    y = 0
-    for c = coeffs
-        y = y * param + c
-    end
-    y
-end
