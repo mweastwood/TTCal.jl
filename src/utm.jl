5adaed47d988dc073d8902809fcb93cb85e36c2d
Michael Eastwood <mweastwood@astro.caltech.edu>
2016-02-03 11:10:11 -0800

    add function for converting azel to itrf
    the CasaCore function to do this is extremely slow and is a limiting
    factor when it comes to accelerating visibility generation

--- src/utm.jl
+++ src/utm.jl
@@ -78,8 +78,7 @@ function latlong_to_utm(zone, latitude, longitude)
 end
 
 """
-    utm_to_latlong(zone, easting, northing,
-                   hemisphere = north)
+    utm_to_latlong(zone, easting, northing, hemisphere = north)
 
 Convert the easting and northing (both in meters) to
 a latitude and longitude (both in degrees).
@@ -128,5 +127,25 @@ function utm_to_latlong{N}(zone, easting, northing,
     latitude *= 180/π
 
     latitude,longitude
+end
+
+"""
+    azel_to_itrf(direction, location)
+
+The conversion from azimuth and elevation to an ITRF direction is
+straightforward. CasaCore's implementation is slow and requires
+unnecessary information (the current time).
+"""
+function direction_to_itrf(direction, location)
+    direction.sys == dir"AZEL" || error("Direction must have the AZEL coordinate system.")
+    location.sys  == pos"ITRF" || error("Location must have the ITRF coordinate system.")
+    z     = [0, 0, 1]
+    loc   = [ location.x,  location.y,  location.z]
+    up    = loc / norm(loc)
+    north = z - dot(z, up)*up
+    east  = cross(north, up)
+    dir   = direction.x * north + direction.y * east + direction.z * up
+    dir  /= norm(dir)
+    Direction(dir"ITRF", dir[1], dir[2], dir[3])
 end
 
